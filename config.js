ad5d1b44bbbe923b09cd2e7c1535c87a20266dc8
Akhil Kumar Sharma <163167559+codebyakhil@users.noreply.github.com>
2025-09-07 23:15:27 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const jpdbBaseURL = "https://api.login2explore.com:5577";
+const jpdbIRL = "/api/irl";
+const jpdbIML = "/api/iml";
+const dbName = "EmployeeDB";
+const relName = "EmployeeData";
+const connToken = "YOUR_API_TOKEN_HERE"; // Replace with your token
